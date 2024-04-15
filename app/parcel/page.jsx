d0bdf785f7c9a10948357784d7257a619246fd85
IsraelChidera <israelchidera54@gmail.com>
2024-04-15 10:49:15 +0100

    Updates on parcel screen

--- app/parcel/page.jsx
+++ app/parcel/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState, useEffect } from 'react';
-import Button from '../';
+import Button from '@/components/Button';
 import Container from '@/components/Container';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
